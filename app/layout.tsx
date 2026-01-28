e1ea808e20d9dc2e51fcb908a6e8a3b84bce5d9c
Mayokun Areola <kareola960@gmail.com>
2026-01-28 12:44:58 +0100

    Update seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.globalskyprint.com"),
+  alternates: {
+    canonical: "/",
+  },
   title: {
     default:
       "The Skyprint Global Services Limited - Corporate Printing & Branding Solutions",
@@ -143,6 +146,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+        <link rel="canonical" href="https://www.globalskyprint.com" />
         <meta
           name="google-site-verification"
           content="vdugVCC7JKvPaf2ifMDLQftTNFFHrRG_uU3-Qv2k_94"
